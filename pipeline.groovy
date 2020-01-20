a43a4fc6809919666429d10968f1524da39ed1fd
dench111 <46183310+dench111@users.noreply.github.com>
2020-01-20 08:52:59 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -7,12 +7,6 @@ pipeline {
     stages {
         stage('build') {
             steps {
-               script {
-                def workspace = "/var/lib/jenkins/workspace/JavaProject_Pipeline/initial"
-                sh "chmod ugo+rwx $workspace/*"
-                sh "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-i386/bin/java"
-                sh "export JRE_HOME=/usr/lib/jvm/java-8-openjdk-i386/jre/bin/java"
-               }
                 sh 'mvn compile -f /var/lib/jenkins/workspace/JavaProject_Pipeline/initial'
                 sh 'mvn package'
             }
