0ec3efcc06d27b6ae7f9eaf78e588d75a6c0cccb
dench111 <46183310+dench111@users.noreply.github.com>
2020-01-20 11:55:52 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,7 +10,9 @@ pipeline {
                 sh 'mvn compile -f /var/lib/jenkins/workspace/JavaProject_Pipeline/initial'
                 sh 'mvn package -f /var/lib/jenkins/workspace/JavaProject_Pipeline/initial'
             }
+        }
            stage('execute jar') {
+              steps {
               sh 'java -jar target/gs-maven-0.1.0.jar'
            }
         }
